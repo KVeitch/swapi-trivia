4f7385ca438835494a932c76a85d6f2818edc389
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-09 22:20:09 -0600

    Add fetching for characters card data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -29,7 +29,71 @@ export const createCharacterList = characters => {
 export const getHomeWorld = homeworld => {
   
 }
-/// Function to get all characters from planet
 
-//Second function to get a single character  will be called in function above 
 
+export const getFilmCharacters = (id) => {
+  let url = `https://swapi.co/api/films/${id}`
+  return fetch(url).then(res => res.json())
+  .then(data => data.characters)
+  .then(data => formatData(data))
+  .then(chars => getCharacterHomeworld(chars))
+  .then(chars => getCharacterSpecies(chars))
+  .then(chars => getCharacterFilms(chars))
+}
+
+const formatData = (characterUrls) => {
+  let charactersData = characterUrls.map(charUrl => {
+    return fetch(charUrl).then(res => res.json()).then(data => {
+      return {
+        films: data.films,
+        homeworld: data.homeworld,
+        name:data.name,
+        species: data.species,
+      }
+    })
+  })
+  return Promise.all(charactersData)
+}
+
+
+const getCharacterSpecies = (chars) => {
+  let speciesData = chars.map( char => {
+    return fetch(char.species).then(res => res.json()).then(species => {
+      return {
+        ...char,
+        species: species.name
+        }
+      })
+    }
+  )
+  return Promise.all(speciesData)
+}
+
+const getCharacterHomeworld = (chars) => {
+  let homeworldData = chars.map(char => {
+    return fetch(char.homeworld).then(res => res.json()).then(homeworld => {
+      return {
+        ...char,
+        homeworld: homeworld.name,
+        population: homeworld.population
+      }
+    })
+  })
+  return Promise.all(homeworldData)
+}
+
+const getCharacterFilms = (chars) => {
+  let films = chars.map(char => {
+    let filmTitles = char.films.map(film => {
+      return fetch(film).then(res => res.json()).then(film => film.title)
+    })
+    return Promise.all(filmTitles).then(filmTitles => {
+      return {
+        ...char,
+        films: filmTitles
+      }
+    }
+    )
+  })
+  return Promise.all(films)
+}
