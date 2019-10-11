65c61d2a4ebb0aca43c24a679bfdab6b906a7e90
Julian Enochs <julianenochs@yahoo.com>
2019-10-10 22:53:33 -0600

    Update C3PO to show insults

---
+++ src/mock-data/C3PO.js
@@ -0,0 +1,10 @@
+const insults = [
+    {quote: 'Don’t call me a mindless philosopher, you overweight glob of grease!'},
+    {quote: 'After all, he’s only a Wookiee.'},
+    {quote: 'I can’t abide these Jawas. Disgusting creatures.'},
+    {quote: 'You’ll be malfunctioning within a day, you nearsighted scrap pile.'},
+    {quote: 'Wait. Oh my! What have you done. I’m backwards. You flea-bitten furball! Only an overgrown mop-head like you would be stupid enough to…'},
+    {quote: 'Don’t worry about Master Luke. I’m sure he’ll be all right. He’s quite clever, you know… for a human being.'}
+]
+
+export default insults;
