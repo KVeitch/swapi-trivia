4473cc1c1f317294beeffaec1ff13503dd84da60
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-13 14:10:15 -0600

    Add testing for CharacterCard and Form components

--- src/CharacterCard/CharacterCard.test.js
+++ src/CharacterCard/CharacterCard.test.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import CharacterCard from './CharacterCard';
 
 describe('Character Card', () => {
-  let wrapper, mockCharacter;
+  let wrapper;
+  const mockSetFav = jest.fn();
+  const mockFavoriteList = []
+  const mockCharacter = {
+                          films:['Best of my Home movies', 'Mostly vines'],
+                          homeworld:'earth',
+                          name:'Bob',
+                          population:'8000000000',
+                          species:'humanish',
+                        }
+
   beforeEach(()=> {
-    mockCharacter = {
-      films:['Best of my Home movies', 'Mostly vines'],
-      homeworld:'earth',
-      name:'Bob',
-      population:'8000000000',
-      species:'humanish',
-    }
-    const mockSetFav = jest.fn();
-    const mockFavoriteList = []
     wrapper = shallow(<CharacterCard 
       key={377} 
       character={mockCharacter}
@@ -25,4 +26,18 @@ describe('Character Card', () => {
   it('should match the snapshot with all the data passed in correctly', () => {
       expect(wrapper).toMatchSnapshot();
   });
-});
+
+  it('Should call setFavorite when the button is clicked', ()=>{
+    wrapper = mount(<CharacterCard 
+      key={377} 
+      character={mockCharacter}
+      setFavorite={mockSetFav}
+      favoriteList={mockFavoriteList}
+      />);
+    // wrapper.update()
+    wrapper.find('.div__favorite').simulate('click');
+      console.log(wrapper.props())
+    expect(wrapper.props().setFavorite).toHaveBeenCalled()
+  
+  })
+}); 
--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Form from './Form';
-import { format } from 'url';
 
 describe('Form', () => {
     let wrapper;
