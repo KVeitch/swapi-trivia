3c42a3107543818c27f8ec60add6f78c7331d50a
Julian Enochs <julianenochs@yahoo.com>
2019-10-13 12:49:39 -0600

    Style buttons to be on bottom

--- src/UserMenu/UserMenu.js
+++ src/UserMenu/UserMenu.js
@@ -4,15 +4,19 @@ import './UserMenu.css';
 
 const UserMenu = (props) => {
     return(
+        <div className='footer'>
         <div className='user__box'>
             <p className='box__ends'>{props.user}</p>
             <p>Favorite Quote: "{props.userQuote}"</p>
             <p className='box__ends'>Ranking: {props.userRanking}</p>
-            <Link to='/movies/'><button className='user__buttons' onClick={props.resetIsCurrentCharacterLoaded}>Back To Movies</button></Link>
-            <Link to='/favorites/'><button className='user__buttons'>Favorite Characters</button></Link>
-            <Link to ='/'>
-                <button className='user__buttons' onClick={props.signUserOut}>Sign Out</button>
-            </Link>
+        </div>
+            <div className='buttons'>
+                <Link to='/movies/'><button className='user__buttons' onClick={props.resetIsCurrentCharacterLoaded}>Back To Movies</button></Link>
+                <Link to='/favorites/'><button className='user__buttons'>Favorite Characters</button></Link>
+                <Link to ='/'>
+                    <button className='user__buttons' onClick={props.signUserOut}>Sign Out</button>
+                </Link>
+            </div>
         </div>
     )
 }
