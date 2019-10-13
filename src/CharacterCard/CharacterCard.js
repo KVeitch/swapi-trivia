17ad3731755317b6bb059997ebb67d551af45f2b
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-13 17:21:37 -0600

    Add a favorite star to character cards

--- src/CharacterCard/CharacterCard.js
+++ src/CharacterCard/CharacterCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './CharacterCard.css'
-
+import favoriteStar from '../images/favorite.svg';
+import star from '../images/star.svg'
 const CharacterCard = props => {
 
   const { films, homeworld, name, population, species} = props.character;
   const classList = props.favoriteList && props.favoriteList.includes(name) ? 'character__card favorite' : 'character__card';
+  const starIcon = props.favoriteList && props.favoriteList.includes(name) ? favoriteStar : star;
 
   const filmList = films.map(film => <li>{film}</li>)
   return(
     <div className={classList}>
       <h2 className='character__name'>{name}</h2>
+      <img className='favorite__icon' src={starIcon} alt='star icon indicatin selected favorite'/>
       <ul className='character__data__list'>
           <li className='character__species'>Species: {species}</li>
           <li className='character__homeworld'>Homeworld: {homeworld}</li>
