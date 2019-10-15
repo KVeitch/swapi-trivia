826b4ede0925d94f775fa0948b67bb6c28e8a978
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-15 13:20:46 -0600

    Updates

---
+++ precache-manifest.e1fb2275c30e3c8d6aac3927082359f2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad45d8383138cec772d9f87469b006cf",
+    "url": "/swapi-trivia/index.html"
+  },
+  {
+    "revision": "46cfbb56ac85b0819744",
+    "url": "/swapi-trivia/static/css/main.568297b1.chunk.css"
+  },
+  {
+    "revision": "33c914b7c662e4c9cbb6",
+    "url": "/swapi-trivia/static/js/2.368274be.chunk.js"
+  },
+  {
+    "revision": "46cfbb56ac85b0819744",
+    "url": "/swapi-trivia/static/js/main.aa8e999a.chunk.js"
+  },
+  {
+    "revision": "4d91dc8747153c95bca8",
+    "url": "/swapi-trivia/static/js/runtime-main.ac423d25.js"
+  },
+  {
+    "revision": "c10406a4831b6b28d2289f255d2f0882",
+    "url": "/swapi-trivia/static/media/003-robot.c10406a4.svg"
+  },
+  {
+    "revision": "c0210b84c1f37cac96edb73e3621355a",
+    "url": "/swapi-trivia/static/media/favorite.c0210b84.svg"
+  },
+  {
+    "revision": "455cac466d47a6f191c1b1ac0cdef9eb",
+    "url": "/swapi-trivia/static/media/loading.455cac46.gif"
+  },
+  {
+    "revision": "d072b762de234e54b759b3e216a0fb9e",
+    "url": "/swapi-trivia/static/media/star.d072b762.svg"
+  },
+  {
+    "revision": "6c79b0f8924f57dd6cb8f3d6a1930edb",
+    "url": "/swapi-trivia/static/media/tatooinesunset.6c79b0f8.jpg"
+  }
+]);
